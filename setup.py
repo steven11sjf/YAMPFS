06870f255f07201ed5d8dc494952ec63626ded06
Steven Franklin <contact@stevenfranklin.dev>
2026-08-23 22:04:31 -0500

    c

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from setuptools import setup
+import platform
+import subprocess
+
+import setuptools
+from setuptools.command.build_py import build_py
 from setuptools.dist import Distribution
 
+
+class BuildPyCommand(build_py):
+    def run(self):
+        subprocess.run(['dotnet', 'publish', 'YAMPFS-LIB', '-c', 'Release', '-o', 'mpo_yampfs/yampfs', '-r', platform.platform()], check=True)
+        build_py.run(self)
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
-setup(distclass=BinaryDistribution)
+setuptools.setup(
+    cmdclass={
+        "build_py": BuildPyCommand,
+    },
+    distclass=BinaryDistribution,
+)
